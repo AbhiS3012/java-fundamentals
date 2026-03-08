d27b6297e910cfa7c815c7b2e801b356c8b4054b
AbhishekSawant <114408189+AbhiS3012@users.noreply.github.com>
2026-03-08 17:35:15 +0530

    Change loop range

--- src/com/multithreading/Atomic.java
+++ src/com/multithreading/Atomic.java
@@ -13,13 +13,13 @@ public class Atomic {
 
 	public static void main(String[] args) throws InterruptedException {
 		Thread t1 = new Thread(() -> {
-			for (int i = 0; i < 1000; ++i) {
+			for (int i = 0; i < 500; ++i) {
 				count.incrementAndGet();
 			}
 		});
 
 		Thread t2 = new Thread(() -> {
-			for (int i = 0; i < 1000; ++i) {
+			for (int i = 0; i < 500; ++i) {
 				count.incrementAndGet();
 			}
 		});
