d7c09cbfafe56a06abcae00c51e7e79cceef0ffc
AbhishekSawant <114408189+AbhiS3012@users.noreply.github.com>
2026-03-08 15:13:02 +0530

    Add new feature

---
+++ src/com/multithreading/Atomic.java
@@ -0,0 +1,36 @@
+package com.multithreading;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Atomic variables are used to perform thread-safe operations without
+ * synchronization using CAS(compare and swap).
+ * 
+ */
+public class Atomic {
+
+	private static AtomicInteger count = new AtomicInteger(0);
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i < 1000; ++i) {
+				count.incrementAndGet();
+			}
+		});
+
+		Thread t2 = new Thread(() -> {
+			for (int i = 0; i < 1000; ++i) {
+				count.incrementAndGet();
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+
+		System.out.println(count);
+	}
+
+}
